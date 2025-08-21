a42ff952bb3673fa11b4a6bcced375be1bd7af28
Erick Schulz <schulz@plexim.com>
2025-08-21 14:55:06 +0200

    fixed display of latex in the documentation website

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,9 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 export default defineConfig({
   integrations: [
     starlight({
@@ -22,7 +25,7 @@ export default defineConfig({
           items: [
             { label: 'Introduction', link: '/' },
             { label: 'Quickstart', link: '/guides/quickstart/' },
-			{ label: 'Installation', link: '/guides/installation/' },
+            { label: 'Installation', link: '/guides/installation/' },
           ],
         },
         {
@@ -30,6 +33,23 @@ export default defineConfig({
           autogenerate: { directory: 'reference' },
         },
       ],
+
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'stylesheet',
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
+            integrity: 'sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV',
+            crossorigin: 'anonymous',
+          },
+        },
+      ],
     }),
   ],
+
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
